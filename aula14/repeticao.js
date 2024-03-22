64402ecbfd34e68e1e9cdfd0640af879d127e4f9
LeonardoDev-Front <134795128+Leonarpro13452@users.noreply.github.com>
2024-03-22 15:17:11 -0300

    criei a aula14

---
+++ aula14/repeticao.js
@@ -0,0 +1,10 @@
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 100);*/
+console.log('Vai começar...')
+for (var c = 1; c <= 15; c++) {
+    console.log(c)
+}
+console.log("Fim!")
